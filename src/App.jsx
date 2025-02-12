e1085b6e8b8e9fa2bd2973b1666bbc20ac068d7d
Tiger66070167 <139055235+Tiger66070167@users.noreply.github.com>
2025-02-12 10:12:19 +0700

    fix folder

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import HomePage from "./Homepage"
+
+function App(){
+  return <div className="center"><HomePage></HomePage></div>
+}
+
+export default App;
---
+++ src/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HomePage(){
+    return (
+        <div>
+            <h1>Welcome to Our Dental Clinic</h1>
+            <p>Your smile is our priority. Book an appointment today!</p>
+            <button>
+                Book Now
+            </button>
+        </div>
+    );
+};
+
+export default HomePage;
