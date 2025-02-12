902483a51b80ff507ef6d11d896f6238464cb778
teehit <teenoi13758@gmail.com>
2025-02-12 15:29:11 +0700

    update app.jsx
    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
+import HomePage from './Homepage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
